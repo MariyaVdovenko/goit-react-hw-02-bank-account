386ec14f9437b29fc23839302097c28cacfc85e5
MariyaVdovenko <m.vdovenko@ukr.net>
2019-12-01 12:40:25 +0200

    Updates

---
+++ precache-manifest.31dde6f003d619cc1206c8d2f1796fd3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dde2c433a703630244f4e274f4e71730",
+    "url": "/goit-react-hw-02-bank-account/index.html"
+  },
+  {
+    "revision": "469d808247cd791a0176",
+    "url": "/goit-react-hw-02-bank-account/static/css/main.9dc0cbae.chunk.css"
+  },
+  {
+    "revision": "d1823f583d8bcc3f56ba",
+    "url": "/goit-react-hw-02-bank-account/static/js/2.1950d1ec.chunk.js"
+  },
+  {
+    "revision": "469d808247cd791a0176",
+    "url": "/goit-react-hw-02-bank-account/static/js/main.fe4c9a2b.chunk.js"
+  },
+  {
+    "revision": "59b69276754240abb686",
+    "url": "/goit-react-hw-02-bank-account/static/js/runtime-main.611f8d42.js"
+  }
+]);
